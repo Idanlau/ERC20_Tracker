504568bd05a2cdf92ad1b5dbdde2e3a8d3c98036
IdanLau <yl9727@nyu.edu>
2024-02-01 18:04:04 -0500

    edits

--- more_than_10k.py
+++ more_than_10k.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 
 def more_than_10k():
-    
+
     file_path = 'num_holders_tokens_list.csv'
     df = pd.read_csv(file_path)
 
     df['Num Holders'] = pd.to_numeric(df['Num Holders'], errors='coerce')
-    df_filtered = df[df['Num Holders'] >= 10000]
+    df_filtered = df[df['Num Holders'] > 10000]
 
     df_filtered = df_filtered[["Name","Token Address","Num Holders"]] # Save the Name, Token Address and Num Holders
     df_filtered.to_csv('contracts_with_more_than_10k_holders.csv', index=False)
--- worker.py
+++ worker.py
@@ -3,7 +3,7 @@ from token_info import get_num_holders
 from more_than_10k import more_than_10k
 
 get_erc20_tokens() # Get of erc20 tokens lists, outputs in active_tokens_list.csv
-get_num_holders()
-more_than_10k()
+get_num_holders() # Gets number of holders for the token, outputs in num_holders_tokens_list.csv
+more_than_10k() # Gets tokens with more than 10k holders, outputs in contracts_with_more_than_10k_holders.csv
 
 
